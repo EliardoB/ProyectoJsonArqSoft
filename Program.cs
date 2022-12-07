71c070cf4229997db9e5324c47faccd37b2fe7db
EliardoB <115761583+EliardoB@users.noreply.github.com>
2022-12-06 21:50:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Json.Net;
+using Newtonsoft.Json;
+
+namespace ArqSoft1
+{
+    internal class Program
+    {
+        public class student
+        {
+            public string Nombre { get; set; }
+            public string Matricula { get; set; }
+        }
+        public class studenContainer
+        {
+            public List<student> students { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            string jsonPath = @"C:\Users\eluis\source\repos\ArqSoft1\students.json";
+            JsonTextReader reader = new JsonTextReader
+            student st = new student();
+
+            using (StreamReader)
+            student JSONresult = (student)JsonConvert.DeserializeObject(jsonPath);
+
+            Console.WriteLine(JSONresult);
+        }
+
+        
+
+        
+    }
+}
